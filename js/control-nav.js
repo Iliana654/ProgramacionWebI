9db14285658239c84820d3aa04222598615d763d
“iliana654” <ilianazuniga12@gmail.com>
2025-08-12 01:04:50 -0600

    Cuarto commit

--- js/control-nav.js
+++ js/control-nav.js
@@ -3,44 +3,44 @@ $("#p-index").on("click", function () {
     $("#pag-hombres").removeClass("active-on").addClass("active-off");
     $("#pag-mujeres").removeClass("active-on").addClass("active-off");
     $("#pag-historia").removeClass("active-on").addClass("active-off");
-    $("#pag-comprar, #pag-preguntas, #pag-politicas, #pag-metodos, #pag-envios, #pag-atencion").removeClass("active-on").addClass("active-off");
+    $("#pag-comprar, #pag-preguntas, #pag-politicas, #pag-metodos, #pag-envios, #pag-atencion, #pag-detalles-producto").removeClass("active-on").addClass("active-off");
 });
 
 $("#p-hombres, #img-p-hombres").on("click", function () {
     $("#pag-hombres").removeClass("active-off").addClass("active-on");
     $("#pag-principal").removeClass("active-on").addClass("active-off");
-    $("#pag-mujeres , #pag-comprar , #pag-historia, #pag-preguntas , #pag-politicas, #pag-metodos, #pag-envios, #pag-atencion").removeClass("active-on").addClass("active-off");
+    $("#pag-mujeres , #pag-comprar , #pag-historia, #pag-preguntas , #pag-politicas, #pag-metodos, #pag-envios, #pag-atencion, #pag-detalles-producto").removeClass("active-on").addClass("active-off");
 });
 
 $("#p-mujeres, #img-p-mujeres").on("click", function () {
     $("#pag-mujeres").removeClass("active-off").addClass("active-on");
     $("#pag-hombres").removeClass("active-on").addClass("active-off");
-    $("#pag-principal , #pag-comprar , #pag-historia, #pag-preguntas, #pag-politicas, #pag-metodos, #pag-envios, #pag-atencion").removeClass("active-on").addClass("active-off");
+    $("#pag-principal , #pag-comprar , #pag-historia, #pag-preguntas, #pag-politicas, #pag-metodos, #pag-envios, #pag-atencion, #pag-detalles-producto").removeClass("active-on").addClass("active-off");
 });
 
 $("#p-historia").on("click", function () {
     $("#pag-historia").removeClass("active-off").addClass("active-on");
     $("#pag-principal").removeClass("active-on").addClass("active-off");
-    $("#pag-hombres , #pag-mujeres , #pag-comprar, #pag-preguntas , #pag-politicas, #pag-metodos, #pag-envios, #pag-atencion").removeClass("active-on").addClass("active-off");
+    $("#pag-hombres , #pag-mujeres , #pag-comprar, #pag-preguntas , #pag-politicas, #pag-metodos, #pag-envios, #pag-atencion, #pag-detalles-producto").removeClass("active-on").addClass("active-off");
 });
 $("#p-comprar").on("click", function () {
     $("#pag-comprar").removeClass("active-off").addClass("active-on");
     $("#pag-principal").removeClass("active-on").addClass("active-off");
     $("#pag-hombres , #pag-mujeres").removeClass("active-on").addClass("active-off");
-    $("#pag-historia, #pag-preguntas, #pag-politicas, #pag-metodos , #pag-envios, #pag-atencion").removeClass("active-on").addClass("active-off");
+    $("#pag-historia, #pag-preguntas, #pag-politicas, #pag-metodos , #pag-envios, #pag-atencion, #pag-detalles-producto").removeClass("active-on").addClass("active-off");
 });
 $("#p-preguntas").on("click", function () {
     $("#pag-preguntas").removeClass("active-off").addClass("active-on");
     $("#pag-principal").removeClass("active-on").addClass("active-off");
     $("#pag-hombres , #pag-mujeres").removeClass("active-on").addClass("active-off");
-    $("#pag-historia, #pag-comprar , #pag-politicas, #pag-metodos , #pag-envios, #pag-atencion").removeClass("active-on").addClass("active-off");
+    $("#pag-historia, #pag-comprar , #pag-politicas, #pag-metodos , #pag-envios, #pag-atencion, #pag-detalles-producto").removeClass("active-on").addClass("active-off");
 });
 
 $("#p-politicas").on("click", function () {
     $("#pag-politicas").removeClass("active-off").addClass("active-on");
     $("#pag-principal").removeClass("active-on").addClass("active-off");
     $("#pag-hombres , #pag-mujeres").removeClass("active-on").addClass("active-off");
-    $("#pag-historia, #pag-comprar , #pag-preguntas, #pag-metodos, #pag-envios, #pag-atencion").removeClass("active-on").addClass("active-off");
+    $("#pag-historia, #pag-comprar , #pag-preguntas, #pag-metodos, #pag-envios, #pag-atencion, #pag-detalles-producto").removeClass("active-on").addClass("active-off");
 });
 
 
@@ -48,19 +48,19 @@ $("#p-metodos , .nav-metodos").on("click", function () {
     $("#pag-metodos").removeClass("active-off").addClass("active-on");
     $("#pag-principal").removeClass("active-on").addClass("active-off");
     $("#pag-hombres , #pag-mujeres").removeClass("active-on").addClass("active-off");
-    $("#pag-historia, #pag-comprar , #pag-preguntas, #pag-politicas, #pag-envios, #pag-atencion").removeClass("active-on").addClass("active-off");
+    $("#pag-historia, #pag-comprar , #pag-preguntas, #pag-politicas, #pag-envios, #pag-atencion, #pag-detalles-producto").removeClass("active-on").addClass("active-off");
 });
 
 $("#p-envios , .nav-envios").on("click", function () {
     $("#pag-envios").removeClass("active-off").addClass("active-on");
     $("#pag-principal").removeClass("active-on").addClass("active-off");
     $("#pag-hombres , #pag-mujeres").removeClass("active-on").addClass("active-off");
-    $("#pag-historia, #pag-comprar , #pag-preguntas, #pag-politicas , #pag-metodos, #pag-atencion").removeClass("active-on").addClass("active-off");
+    $("#pag-historia, #pag-comprar , #pag-preguntas, #pag-politicas , #pag-metodos, #pag-atencion, #pag-detalles-producto").removeClass("active-on").addClass("active-off");
 });
 
 $("#p-atencion").on("click", function () {
     $("#pag-atencion").removeClass("active-off").addClass("active-on");
     $("#pag-principal").removeClass("active-on").addClass("active-off");
     $("#pag-hombres , #pag-mujeres").removeClass("active-on").addClass("active-off");
-    $("#pag-historia, #pag-comprar , #pag-preguntas, #pag-politicas , #pag-metodos, #pag-envios").removeClass("active-on").addClass("active-off");
+    $("#pag-historia, #pag-comprar , #pag-preguntas, #pag-politicas , #pag-metodos, #pag-envios, #pag-detalles-producto").removeClass("active-on").addClass("active-off");
 });
